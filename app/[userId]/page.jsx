5243e1cbcf37aaf7715aa4204480c810a28b3777
Odirile <motauodirile@gmail.com>
2024-05-28 15:45:51 +0200

    new updates to fix an error

--- app/[userId]/page.jsx
+++ app/[userId]/page.jsx
@@ -1,67 +1,61 @@
 "use client"
-
-
-import React, {useEffect, useState} from 'react'
-import app from '../Shared/firebaseConfig'
+import React, { useEffect,useState } from 'react'
+import app from '../Shared/firebaseConfig';
 import UserInfo from './../components/UserInfo'
+import { collection, getDocs,getDoc,doc, getFirestore, query, where } from 'firebase/firestore'
 import PinList from './../components/Pins/PinList'
-import { query, collection, getDocs, where, getDoc, getFirestore, doc } from 'firebase/firestore'
-
-
-
 
 function Profile({params}) {
   const db=getFirestore(app);
   const [userInfo,setUserInfo]=useState();
-  const[listOfPins,setListOfPins]=useState([]);
+  const [listOfPins,setListOfPins]=useState([]);
   useEffect(()=>{
-    console.log(params.userId.replace('%40', '@'))
-    if(params){
-      getUserInfo(params.userId.replace('%40', '@'))
+    console.log(params.userId.replace('%40','@'))
+    if(params)
+    {
+      getUserInfo(params.userId.replace('%40','@'))
     }
   },[params]);
 
 
   const getUserInfo=async(email)=>{
-   const docRef = doc(db, "user", email);
+    const docRef = doc(db, "user",email );
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      //console.log("Document data:", docSnap.data());
+      
       setUserInfo(docSnap.data())
-    }else{
-      // docSnap.data() willbeundefined in this case
+    } else {
+      // docSnap.data() will be undefined in this case
       console.log("No such document!");
     }
   }
-
-  
-
   useEffect(()=>{
-    if(userInfo){
-      getUserInfo();
-    }
+      if(userInfo)
+      {
+        getUserPins();
+      }
   },[userInfo])
-
   const getUserPins=async()=>{
-      const q = query(collection(db, 'pinterest-post'), where("email", '==', userInfo.email));
+    setListOfPins([])
+      const q=query(collection(db,'pinterest-post')
+      ,where("email",'==',userInfo.email));
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
+     
+      setListOfPins(listOfPins=>[...listOfPins,doc.data()]);
       });
   }
-
   return (
     <div>
-      {userInfo?
+     {userInfo?
      <div>
-      <UserInfo userInfo={userInfo}/>
-
-     <PinList />
-     </div>:null}
-      
-    </div> 
+      <UserInfo userInfo={userInfo} />
+     
+      <PinList listOfPins={listOfPins}  />
+      </div> :null}
+    </div>
   )
 }
 
