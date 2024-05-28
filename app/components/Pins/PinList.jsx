93f6f67052b2c80695756bf1d5ef97c121465844
Odirile <motauodirile@gmail.com>
2024-05-28 17:30:15 +0200

    Able to see different pins

--- app/components/Pins/PinList.jsx
+++ app/components/Pins/PinList.jsx
@@ -2,11 +2,20 @@
 import app from '@/app/Shared/firebaseConfig'
 
 import React, { useEffect } from 'react'
+import PinItem from './PinItem'
 
-function PinList() {
-   
+function PinList({listOfPins}) {
+   console.log(listOfPins)
   return (
-    <div>PinList</div>
+    <div lassName='mt-7 px-2 md:px-5
+    columns-2 md:columns-3
+    lg:columns-4 mb-4
+    xl:columns-5 space-y-6 mx-auto'>
+      {listOfPins.map((items,index)=>(
+        <PinItem pin={items}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
