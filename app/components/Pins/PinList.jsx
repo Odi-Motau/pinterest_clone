2d3a54a9088ce015c28fda0a071d33069d4fe7c3
Odirile <motauodirile@gmail.com>
2024-06-02 20:38:15 +0200

    column

--- app/components/Pins/PinList.jsx
+++ app/components/Pins/PinList.jsx
@@ -7,7 +7,7 @@ import PinItem from './PinItem'
 function PinList({listOfPins}) {
    console.log(listOfPins)
   return (
-    <div lassName='mt-7 px-2 md:px-5
+    <div className='mt-7 px-2 md:px-5
     columns-2 md:columns-3
     lg:columns-4 mb-4
     xl:columns-5 space-y-6 mx-auto'>
